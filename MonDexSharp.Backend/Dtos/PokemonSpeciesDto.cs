673712b13a1c7b02b39b569aa175819b6179efdc
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2025-01-19 16:00:10 -0400

    feat: dto/domain conversion

--- MonDexSharp.Backend/Dtos/PokemonSpeciesDto.cs
+++ MonDexSharp.Backend/Dtos/PokemonSpeciesDto.cs
@@ -1,5 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using MonDexSharp.Core.Entities;
 
 namespace MonDexSharp.Backend.Dtos;
 
-public record PokemonSpeciesDto(int? Id, [Required][MinLength(1)] string Name, PokemonStatsDto Stats);
+public record PokemonSpeciesDto(int Id, [Required][MinLength(1)] string Name, PokemonStatsDto BaseStats)
+{
+    public PokemonSpeciesDto(PokemonSpecies entity) : this(entity.Id, entity.Name, new(entity.BaseStats)) { }
+
+    public PokemonSpecies ToEntity()
+    {
+        return new(Id, Name, BaseStats.ToValueObject());
+    }
+}
--- MonDexSharp.Backend/Dtos/PokemonStatsDto.cs
+++ MonDexSharp.Backend/Dtos/PokemonStatsDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using MonDexSharp.Core.ValueObjects;
 
 namespace MonDexSharp.Backend.Dtos;
 
@@ -15,4 +16,12 @@ public record struct PokemonStatsDto(
     int SpecialDefense,
     [Range(0, int.MaxValue)]
     int Speed
-);
+)
+{
+    public PokemonStatsDto(PokemonStats src) : this(src.HP, src.Attack, src.Defense, src.SpecialAttack, src.SpecialDefense, src.Speed) { }
+
+    public readonly PokemonStats ToValueObject()
+    {
+        return new(HP, Attack, Defense, SpecialAttack, SpecialDefense, Speed);
+    }
+}
