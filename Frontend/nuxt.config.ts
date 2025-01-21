77d2c49d1efc1ac8b4cacf0d4982472f10e3fead
FluffyBucketSnake <fluffybucketsnake@outlook.com>
2025-01-21 15:55:01 -0400

    misc/frontend: configure some deps

--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["nuxt-open-fetch", "vuetify-nuxt-module"],
+  runtimeConfig: {
+    public: {
+      openFetch: {
+        backend: { baseURL: "http://localhost:5161" },
+      },
+    },
+  },
+  openFetch: {
+    clients: {
+      backend: { baseURL: "http://localhost:5161" },
+    },
+  },
+} as any);
+
