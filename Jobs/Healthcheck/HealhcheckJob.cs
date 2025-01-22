49e036f2e8740986c2ffb45297202c1af2880662
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2025-01-21 21:28:26 -0400

    feat/healthcheck: logging

--- Jobs/Healthcheck/HealhcheckJob.cs
+++ Jobs/Healthcheck/HealhcheckJob.cs
@@ -1,21 +1,25 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Logging;
 
 namespace MonDexSharp.Jobs.Healthcheck;
 
-public class HealthcheckJob(HealthcheckJobConfiguration Configuration)
+public class HealthcheckJob(ILogger<HealthcheckJob> logger, HealthcheckJobConfiguration Configuration)
 {
     public async Task Run(string TargetUri)
     {
+        logger.LogInformation("Starting healthcheck for URI \"{URI}\"", TargetUri);
         HttpClient httpClient = new();
 
         HttpResponseMessage response = await httpClient.SendAsync(new(HttpMethod.Get, TargetUri));
         if (response.StatusCode == HttpStatusCode.OK &&
             await response.Content.ReadAsStringAsync() != "Healthy")
         {
+            logger.LogInformation("URI \"{URI}\" is healthy", TargetUri);
             return;
         }
 
+        logger.LogInformation("URI \"{URI}\" is unhealthy", TargetUri);
         MailMessage message = new(
             Configuration.From,
             Configuration.To)
