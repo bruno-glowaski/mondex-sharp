1428f0beeffc91ae121f9be4edf03ff648106212
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2025-02-26 17:45:10 -0400

    fix: query params not included

--- Frontend/server/api/backend/[...slug].ts
+++ Frontend/server/api/backend/[...slug].ts
@@ -1,6 +1,9 @@
+const PREFIX = "/api/backend/";
 export default defineEventHandler((event) => {
-  const path = event.context.params?.slug;
+  const path = event.path.slice(PREFIX.length);
+  const query = getQuery(event);
   const backendUrl = useRuntimeConfig().public.backend.baseURL;
-  console.log("Redirecting", path, "to", backendUrl);
-  return proxyRequest(event, `${backendUrl}/${path}`);
+  const finalUrl = `${backendUrl}/${path}`;
+  console.log("Redirecting", path, "to", finalUrl);
+  return proxyRequest(event, finalUrl);
 });
